64b7e3f6a997599db85d93430b6c4f3127410c83
Elvi5-40-NTC-TWP <chairezbird.8@gmail.com>
2026-07-09 02:27:17 -0500

    Done with the adding the basic requirement of making the name and description required. Plus I added a limit to the number field to zero. Finally, I made the select of a service, fill in its name in the respective field, and make the buttons worked as intended -- create/save/delete = submit and deselect = reset.

---
+++ AdminUI/JavaScript/AdminSerManage.js
@@ -0,0 +1,19 @@
+//  Startup
+document.addEventListener("DOMContentLoaded", () => {
+    // Target the list items already produced by Admin.js
+    const Button_List = document.querySelectorAll('.scroll-list-box ul li');
+
+    Button_List.forEach((li, index) => {
+        const Button = li.querySelector('button');
+        Button.onclick = function() {Service_Selected(Button);};
+    });
+});
+
+// Functions
+function Service_Selected(Service_Button) 
+{
+    const Text_Field = document.getElementById('name-field');
+    const Service_Name = Service_Button.textContent.trim();
+
+    Text_Field.value = Service_Name;
+}
