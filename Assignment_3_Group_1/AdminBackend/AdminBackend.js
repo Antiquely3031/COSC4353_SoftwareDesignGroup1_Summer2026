45dcdf537ddff99a0fe1fca64c391c9445cfdd71
Elvi5-40-NTC-TWP <chairezbird.8@gmail.com>
2026-07-20 15:51:10 -0500

    Just created the skeletal folder structure to put our files in for Assignment 3; right now, it's just a JS file so that I can make a commit to my branch, then merge into the main.

---
+++ Assignment_3_Group_1/AdminBackend/AdminBackend.js
@@ -0,0 +1,3 @@
+// I, Elvis Noel Trujillo Chairez, got the admin portion of the project as shown in assignment 2.
+// This is just a placeholder for the repo; so that y'all can place your files without making a mess
+// in the repo.
