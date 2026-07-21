cdf45247dfd61726b59a6ab1bdbc656a0dfb0792
Elvi5-40-NTC-TWP <chairezbird.8@gmail.com>
2026-07-20 19:18:06 -0500

    Just installed and played around a bit with Jest (unit testing software). Also, for the time being I just deleted the "Testing" Folder, and I have everything in the "AdminBackend" Folder together to make sure that the coverage folder actually shows any %.

--- Assignment_3_Group_1/AdminBackend/AdminBackend.js
+++ Assignment_3_Group_1/AdminBackend/AdminBackend.js
@@ -1,3 +1,8 @@
 // I, Elvis Noel Trujillo Chairez, got the admin portion of the project as shown in assignment 2.
 // This is just a placeholder for the repo; so that y'all can place your files without making a mess
 // in the repo.
+
+function sum(a, b) {
+  return a + b;
+}
+module.exports = { sum };
