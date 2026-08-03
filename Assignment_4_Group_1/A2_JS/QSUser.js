28b4fb4566983a62f9a479cf1b00387e868eb7b1
kchau <kchau155@gmail.com>
2026-08-02 19:32:30 -0500

    Update QSUser.js

--- Assignment_4_Group_1/A2_JS/QSUser.js
+++ Assignment_4_Group_1/A2_JS/QSUser.js
@@ -4,8 +4,9 @@ const baseAPI = "http://localhost:3000/api";
 // Placeholder user ID because there is no real database/authentication yet
 //const userID = 1;
 
-//temporary,until Patrick finishes with the userCredentials, if failure defaults back to 1
-const userID = Number(sessionStorage.getItem("userID")) || 1;
+//temporary,until Patrick finishes with the userCredentials, if failure defaults back to 2
+//ive inserted a test user in the database remove it later when finalizing the project
+const userID = Number(sessionStorage.getItem("userID")) || 2;
 
 // Mock service data.
 // Later, this can come from GET /api/services.
